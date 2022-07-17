4b55a32b44de826b14395b6ee13ec19bc75d11df
pavel359 <pavel.malchuganov@mail.ru>
2022-07-18 00:01:51 +0600

    minor changes

--- botLogic/createLog.mjs
+++ botLogic/createLog.mjs
@@ -9,12 +9,9 @@ const __dirname = dirname(__filename);
 function createLog () {
     let date = []
     date[0] = new Date().getDate()
-    console.log(new Date().getDate())
-    console.log(date[0])
     date[1] = new Date().getMonth() + 1
     date[2] = new Date().getFullYear()
     date = date.join('-')
-    console.log(date)
     let time = []
     time[0] = new Date().getHours()
     time[1] = new Date().getMinutes()
